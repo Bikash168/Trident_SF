af544db80382ba727db4098688ad4c77510fd98d
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2024-10-08 10:43:17 +0530

    new changes

--- pages/team.tsx
+++ pages/team.tsx
@@ -86,9 +86,9 @@ const OurTeam = () => {
     <ImageCollage />
       <section className="bg-white py-12 mb-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">
-            Meet Our Management Team
-          </h2>
+                   <h2 className="text-4xl font-extrabold text-center  text-blue-500 mb-8">
+                 Meet Our <span className="text-black">Leaders</span>
+                </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             {ManagementteamMembers.map((member, index) => (
               <div key={index} className="bg-gray-100 p-6 rounded-lg shadow-lg text-center transition-transform transform hover:scale-105">
@@ -105,9 +105,9 @@ const OurTeam = () => {
           </div>
         </div>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
-          <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">
-            Meet Our Executive Team
-          </h2>
+                   <h2 className="text-4xl font-extrabold text-center  text-blue-500 mb-8">
+          Meet Our  <span className="text-black">Executive Team</span>
+                </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             {ExicutieteamMembers.map((member, index) => (
               <div key={index} className="bg-gray-100 p-6 rounded-lg shadow-lg text-center transition-transform transform hover:scale-105">
