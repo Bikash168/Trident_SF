eb456314c47e5ffbfefabef713df34394f24a526
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2024-11-08 15:08:07 +0530

    new

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,28 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          {/* External CSS and font styles */}
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+          
+          {/* Meta tags for responsiveness and SEO */}
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta name="description" content="Sustainable Action for Climate Resilient Enviro-social Development Foundation, driving environmental and social sustainability." />
+          <meta name="keywords" content="SACRED Foundation, climate resilience, environmental sustainability, social development" />
+          <meta name="author" content="SACRED Foundation" />
+          
+          {/* Meta tags for performance */}
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
+          <link rel="dns-prefetch" href="https://cdnjs.cloudflare.com" />
+          
+          {/* Caching and loading optimizations */}
+          <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+          <meta http-equiv="Pragma" content="no-cache" />
+          <meta http-equiv="Expires" content="0" />
 
-                   {/* You can add more stylesheets here */}
+          {/* You can add more stylesheets or font links here */}
         </Head>
         <body>
           <Main />
