7da65b866b270e7b041d0ffbff7c809dd5a1540f
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2025-01-10 11:24:16 +0530

    new

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,9 @@
-// pages/_document.tsx
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
+      
       <Html lang="en">
         <Head>
           {/* External CSS and font styles */}
@@ -28,6 +28,7 @@ class MyDocument extends Document {
           {/* Verification Meta Tags */}
           <meta name="p:domain_verify" content="5738d95c5714a031b53b87ec88dddf7a" />
           <meta name="google-site-verification" content="KqTEK616JhBYeZiOhfrTYJg33saYlSKiHjPJNrs0HMk" />
+
           {/* Open Graph Meta Tags */}
           <meta property="og:title" content="SACRED Foundation - Sustainable Action for Climate Resilience" />
           <meta property="og:description" content="Driving environmental and social sustainability for a resilient future." />
@@ -35,6 +36,7 @@ class MyDocument extends Document {
           <meta property="og:url" content="https://www.sacred.foundation" />
           <meta property="og:type" content="website" />
           <meta property="og:locale" content="en_US" />
+
           {/* Twitter Card Meta Tags */}
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@SACREDFoundation" />
@@ -44,6 +46,7 @@ class MyDocument extends Document {
 
           <meta name="robots" content="index, follow" />
 
+          {/* Theme and app meta */}
           <meta name="theme-color" content="#ffffff" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="apple-mobile-web-app-title" content="SACRED Foundation" />
@@ -53,25 +56,24 @@ class MyDocument extends Document {
           <meta http-equiv="Content-Language" content="en" />
           <meta name="language" content="English" />
 
-          <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com; style-src 'self' https://cdnjs.cloudflare.com" />
-
+         
+          {/* Favicons and app icons */}
           <link rel="icon" href="/favicon.ico" />
           <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
           <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
+          {/* Web App Capabilities */}
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="mobile-web-app-capable" content="yes" />
-          {/* Preload Resources */}
-          <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" as="style" />
+
+              {/* Preload fonts correctly */}
+              <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" as="style" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />
+
+          {/* Preload scripts only if used */}
           <link rel="preload" href="/path/to/important-script.js" as="script" />
-
-          <meta http-equiv="Feature-Policy" content="microphone 'none'; camera 'none'" />
-
-
-
-
+          <script src="/path/to/important-script.js" />
         </Head>
         <body>
           <Main />
