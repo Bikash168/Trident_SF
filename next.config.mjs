70e68cb8cca511f36582498c6e1ab8b9414425b3
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2025-01-11 12:01:41 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Match all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; frame-src 'self' https://www.youtube.com; script-src 'self' 'unsafe-inline' https://www.youtube.com; object-src 'none';",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
